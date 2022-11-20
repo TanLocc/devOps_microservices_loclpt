0b42943a4247688a3cf25c4e8b5b247351eb0772
Ubuntu <ubuntu@ip-172-31-77-62.ec2.internal>
2022-11-20 16:10:52 +0000

    add code

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath=0352730247/loclptapp
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run loclptapp\
+    --image=$dockerpath\
+    --port=80\
+    --labels="app=loclptapp"
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
 
+kubectl port-forward loclptapp 8000:80
